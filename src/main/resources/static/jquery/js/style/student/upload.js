39749c565700cfb79c77c716a18cd7689e1c0582
yangkun <1149642605@qq.com>
2020-05-09 22:09:57 +0800

    限制文件上传类型和大小

--- src/main/resources/static/jquery/js/style/student/upload.js
+++ src/main/resources/static/jquery/js/style/student/upload.js
@@ -8,10 +8,20 @@ $(function () {
     });
     // 保存按钮
     $('#uplodProjectForm').on('submit', function(){
+        var file = $('#uplodProjectForm_file')[0].files[0];
+        var fileType = file.name.substring(file.name.lastIndexOf('.')+1);
+        if(fileType !== 'rar' && fileType !== 'zip'){
+            dialog('只能上传rar或zip压缩包');
+            return false;
+        }
+        var fileSize = file.size;
+        if(fileSize > 209715200){
+            dialog('只能上传200MB以内压缩包');
+            return false;
+        }
         var formData = new FormData();
         formData.append("name",$('#uplodProjectForm_name').val());
-        formData.append("file",$('#uplodProjectForm_file')[0].files[0]);
-        console.log(formData);
+        formData.append("file",file);
         upload_projectReport(formData);
         return false;
     })
